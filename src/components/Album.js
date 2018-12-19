a937a918f4bd37df48b57f5e7613f4ef0f96d9d5
Sarah Rothwell <sarahhrothwell@gmail.com>
2018-12-19 18:52:11 -0500

    ComponentsAlbum2

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,18 +1,18 @@
-import React, {Component} from 'react';
-import albumData from './../data/albums.js';
+import React, { Component } from 'react';
+import albumData from './../data/albums';
 
 class Album extends Component {
   constructor(props){
     super(props);
-
     const album = albumData.find( album => {
       return album.slug === this.props.match.params.slug
     });
+
     this.state = {
       album: album
     };
   }
-  
+
   render(){
     return (
       <section className="album">
@@ -24,8 +24,24 @@ class Album extends Component {
             <div id='release-info'>{this.state.album.releaseInfo}</div>
           </div>
         </section>
+        <table id="song-list">
+          <colgroup>
+            <col id="song-number-column"/>
+            <col id="song-title-column"/>
+            <col id="song-duration-column"/>
+          </colgroup>
+          <tbody>
+            { this.state.album.songs.map((song, index) =>
+            <tr key={index}>
+              {index+1}. Title: {song.title}, Duration: {song.duration} seconds
+            </tr>
+            )
+            }
+          </tbody>
+        </table>
       </section>
     );
+
   }
 }
 
